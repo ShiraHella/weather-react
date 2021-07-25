18489277db948f1f4c422cec0d0e6bd26d428399
ShiraHella <buckbeak@Buckbeak.local>
2021-07-26 05:33:54 +0900

    moved onChange from button to search bar

--- src/Weather.js
+++ src/Weather.js
@@ -47,10 +47,11 @@ let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=60dbe
                         placeholder="Enter a city"
                         className="form-control"
                         autoFocus="on"
+                        onChange={handleCityChange}
                     />
               </div>
                     <div className="col-3">
-                      <input className="btn btn-primary w-100" type="submit" value="Search" onChange={handleCityChange}/>
+                      <input className="btn btn-primary w-100" type="submit" value="Search" />
                     </div>
             </div>
       </form>

1ec4efd66631ad5c2794df75cee75179d45ad17a
ShiraHella <buckbeak@Buckbeak.local>
2021-07-25 18:33:20 +0900

    fixed image link

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ const [city, setCity] = useState (props.defaultCity);
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
-    icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`})
+    icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`})
 
   }
 

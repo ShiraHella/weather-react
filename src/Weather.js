5b0de5639fd5703ffa52f2be473cf62d886d92e6
ShiraHella <buckbeak@Buckbeak.local>
2021-07-25 09:03:16 +0900

    Incorporated API into Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
-import SearchEngine from "./SearchEngine";
+import axios from "axios";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Yokohama",
-    temperature: 22,
-    date: "Saturday, 10:00",
-    description: "Rainy",
-    humidity: 95,
-    wind: 10
-  };
 
+  const [weatherData, setWeatherData] = useState({ result: false});
+
+  function handleResponse(response) {
+    setWeatherData({
+      result: true,
+      city: response.data.name,
+      date: `Sunday 10:00`,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`})
+
+  }
+  if (weatherData.result) {
   return (
     <div className="Weather">
       <div className="container">
@@ -23,16 +30,22 @@ export default function Weather() {
                 <div className="today">{weatherData.date}</div>
               </div>
               <div className="col-5 search">
-                <SearchEngine />
+                <form>
+                    <input
+                        type="search"
+                        placeholder="Enter a city"
+                    />
+                    <input type="submit" value="Search" />
+                </form>
               </div>
 
               <div className="col-3 current-weather">
-                <img src={Rainy} alt="weather icon" />
+                <img src={weatherData.icon} alt={weatherData.description} />
               </div>
               <div className="col-5 center-weather">
-                <div className="weather-word">{weatherData.description}</div>
+                <div className="weather-word  text-capitalize">{weatherData.description}</div>
                 <h2 className="card-subtitle mb-2 day-temp">
-                  {weatherData.temperature}°c
+                  {Math.round(weatherData.temperature)}°c
                 </h2>
               </div>
 
@@ -42,7 +55,7 @@ export default function Weather() {
                 </span>
                 <br />
                 <span className="extra-info">
-                  Wind: {weatherData.wind} m/hr
+                  Wind: {Math.round(weatherData.wind)} km/h
                 </span>
               </div>
             </div>
@@ -67,4 +80,11 @@ export default function Weather() {
       </div>
     </div>
   );
+  } else {
+    let city = "Yokohama";
+     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=60dbe083627851751ca64015719aa9ec&units=metric`;
+        axios.get(url).then(handleResponse);
+
+        return "Loading...";
+  }
 }
